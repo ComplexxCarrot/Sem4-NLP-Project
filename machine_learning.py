c99043e13aede643b38e0663ae2c2ce3297fce2f
Carrotkun <43902776+ComplexxCarrot@users.noreply.github.com>
2022-06-20 18:53:41 +0700

    New Changes

--- machine_learning.py
+++ machine_learning.py
@@ -7,38 +7,18 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 
 # using TF-IDF text classification
-
 count_vect = CountVectorizer()
 
 def run_ML():
-  print('Machine starts learning...')
-  
-  url = 'https://raw.githubusercontent.com/ComplexxCarrot/Sem4-NLP-Project/main/spam-datasets.csv'
-  df = pd.read_csv(url, encoding= 'iso-8859-1')
-  df = df[['v1','v2']]
-  print('data taken...')
-  
-  # remove nan (not available) values
-  df.dropna(subset = ["v2"], inplace=True)
+	url = 'https://gist.githubusercontent.com/agtbaskara/a1a7017027cc1df9d35cf06e1e5575b7/raw/59870e27ca217d77ac0d8d8dc100551c0dcd14b3/dataset_sms_spam_v2.csv'
+	df = pd.read_csv(url, encoding= 'utf-8')
+	# df['label'] = df['label'].replace(['promo','penipuan'],['spam','spam'])
+	df = pd.read_csv(url, encoding= 'iso-8859-1')
+	X_train = df['Teks']
+	y_train = df['label']
 
-  print('nan values removed...')
-
-  X_train = df['v2']
-  y_train = df['v1']
-
-  print('training data runs...')
-  
-  X_train_counts = count_vect.fit_transform(X_train)
-
-  print('count vectorizer done...')
-  
-  tfidf_transformer = TfidfTransformer()
-  X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-
-  print('transform to tfidf done...')
-  
-  clf = MultinomialNB().fit(X_train_tfidf, y_train)
-
-  print('machine done learning, saving training results...')
-  
-  return clf
+	X_train_counts = count_vect.fit_transform(X_train)
+	tfidf_transformer = TfidfTransformer()
+	X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+	clf = MultinomialNB().fit(X_train_tfidf, y_train)
+	return clf
